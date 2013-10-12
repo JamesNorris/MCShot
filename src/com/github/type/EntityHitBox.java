4cc92143b9e46667e89c056b4923fbac2bfad635
JamesNorris <Jnorris700@gmail.com>
2013-10-12 08:49:42 -0400

    Added EntityHitBox

---
+++ src/com/github/type/EntityHitBox.java
@@ -0,0 +1,23 @@
+package com.github.type;
+
+import org.bukkit.Location;
+import org.bukkit.entity.Entity;
+
+import com.github.HitBox;
+
+public class EntityHitBox extends HitBox {
+    private Entity entity;
+
+    public EntityHitBox(Entity entity, Location origin, Location center, float yawRotation) {
+        super(origin, center, yawRotation);
+    }
+    
+    public Entity getEntity() {
+        return entity;
+    }
+    
+    @Override public void update() {
+        move(entity.getLocation());
+        rotate(entity.getLocation().getYaw() - getYawRotation());
+    }    
+}
